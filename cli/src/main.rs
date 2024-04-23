c092b6aad5a9b791ae4016ba5067ce31fa159761
timS <lbdl_kb@protonmail.com>
2024-04-23 11:24:53 +0100

    Comment out the redundant main file

---
+++ cli/src/main.rs
@@ -0,0 +1,10 @@
+// use notcurses::*;
+//
+// fn main() -> Result<()> {
+//     let mut nc = Notcurses::new_cli()?;
+//     let mut cli = nc.cli_plane()?;
+//     cli.putstrln("\nhello world!")?;
+//     cli.render()?;
+//     Ok(())
+// }
+fn main() {}
